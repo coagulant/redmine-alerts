b07a327fad7d31ce337caae0200c3e2e361c493b
coagulant <baryshev@gmail.com>
2013-07-02 01:08:43 +0400

    Changed Redmine wrapper api to allow custom get queries

--- redmine_alerts/api.py
+++ redmine_alerts/api.py
@@ -1,28 +1,48 @@
 # coding: utf-8
-from functools import partial
 from hammock import Hammock
 
 
-class Redmine(object):
-    """ Redmine API wrapper with generator for pagination handling"""
+class Redmine(Hammock):
+    """ Redmine API wrapper with generator for pagination handling
 
-    def __init__(self, url, api_key):
-        self.api = Hammock(url, headers={'Content-Type': 'application/json', 'X-Redmine-API-Key': api_key})
+        Example usage:
+            api = Redmine.api('http://example.com', 'ThisIsMyToken')
 
-    def __getattr__(self, name):
-        return partial(self._paginator, '%s.json' % name)
+            >> groups = api.groups.GET()
+            /groups.json
+            >> entries = api.issues.GET(params={'query_id': '42'})
+            /issues.json?query_id=42
+            >> entry = api.issues('2').GET()
+            /issues/2.json
 
-    def _paginator(self, uri, method='GET', data=None):
+        See reference of api: http://www.redmine.org/projects/redmine/wiki/Rest_api
+    """
+
+    @classmethod
+    def api(cls, url, api_key):
+        return cls(url, headers={'Content-Type': 'application/json', 'X-Redmine-API-Key': api_key})
+
+    def _url(self, *args):
+        return super(Redmine, self)._url(*args) + '.json'
+
+    def _request(self, method, *args, **kwargs):
+        if method == 'get':
+            return self._paginator(method, self._url(*args), **kwargs)
+        else:
+            return self._session.request(method, self._url(*args), **kwargs)
+
+    def _paginator(self, method, uri, **kwargs):
         offset, limit = 0, 1
         while uri and offset is not None:
-            api_items, limit, offset = self._request(uri, method, data, limit=limit, offset=offset)
+            api_items, limit, offset = self._page_request(method, uri, limit=limit, offset=offset)
             for item in api_items:
                 yield item
 
-    def _request(self, uri, method, data=None, limit=25, offset=0):
-        api_call = getattr(self.api(uri), method)
-        response = api_call(data=data, params={'limit': limit, 'offset': offset}).json()
-        objects = response[uri.split('.')[0]]  # users.json -> users
+    def _page_request(self, method, uri, limit, offset, **kwargs):
+        kwargs.setdefault('params', {})
+        kwargs['params'].update({'limit': limit, 'offset': offset})
+        response = self._session.request(method, uri, **kwargs).json()
+        objects = response[uri[:-5].rsplit('/')[-1]]  # http://example.com/users.json -> users
         next_offset = limit + offset
         if offset + limit < response['total_count']:
             return objects, limit, next_offset
--- redmine_alerts/test_alerts.py
+++ redmine_alerts/test_alerts.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-from .api import Redmine
 import httpretty
+from .api import Redmine
 
 
 @httpretty.activate
@@ -12,8 +12,8 @@ def test_api_wrapper():
                                httpretty.Response('{"groups":[{"id":2, "name":"Robochicken"}], "total_count": 2, "offset": 1, "limit": 1}'),
                            ],
                            content_type="application/json")
-    api = Redmine('http://example.com', 'ThisIsMyToken')
-    entries = api.groups()
+    api = Redmine.api('http://example.com', 'ThisIsMyToken')
+    entries = api.groups.GET()
 
     assert list(entries) == [{'id': 1, 'name': 'Pokemon'}, {'id': 2, 'name': 'Robochicken'}]
-    assert httpretty.HTTPretty.last_request.querystring == {'limit':['1'], 'offset': ['1']}
+    assert httpretty.HTTPretty.last_request.querystring == {'limit': ['1'], 'offset': ['1']}
